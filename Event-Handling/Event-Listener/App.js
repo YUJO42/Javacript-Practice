b4bb081227ff8953d8d60aa0f920767cb11c7d3c
yujo <yujo@student.42seoul.kr>
2020-11-17 17:31:37 +0900

    select check

---
+++ Event-Handling/Event-Listener/App.js
@@ -0,0 +1,10 @@
+const $qeurySelectLi = document.querySelector('li');
+const $qeurySelectAllLi = document.querySelectorAll('li');
+
+const $classNameLi = document.getElementsByClassName('1');
+const $tagNameLi = document.getElementsByTagName('li');
+
+console.log($qeurySelectLi);
+console.log($qeurySelectAllLi);
+console.log($classNameLi);
+console.log($tagNameLi);
