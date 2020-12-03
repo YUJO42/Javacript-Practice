4b23165c1201a045f3bf380567a07416395c7cc3
yujo <yujo@student.42seoul.kr>
2020-12-03 19:31:23 +0900

    test: NodeList ForEach test

--- DOM/index.js
+++ DOM/index.js
@@ -1,5 +1,7 @@
 const $all = document.querySelectorAll('h1');
 console.log($all);
-for (const item of $all) {
-  item.style.display = 'none';
-}
+
+$all.forEach((item) => (item.style.display = 'none'));
+// for (const item of $all) {
+//   item.style.display = 'none';
+// }
