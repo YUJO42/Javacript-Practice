6fe4c9c64abf11af8bec50bf2c55c27d4a627da0
yujo <yujo@student.42seoul.kr>
2020-12-26 08:39:14 +0900

    feat: 페이지 수만큼 버튼을 렌더하는 기능

--- jw-paginate/src/index.js
+++ jw-paginate/src/index.js
@@ -1,6 +1,6 @@
-const dummy = [];
-for (let i = 1; i <= 100; i++) {
-  dummy.push({
+const items = [];
+for (let i = 1; i <= 12; i++) {
+  items.push({
     id: i,
     data: `dummy data ${i}`,
   });
@@ -15,15 +15,32 @@ const tableInfoTemplate = ({ id, data }) => {
 
 const renderTable = () => {
   const $mainTable = document.querySelector('.main-table');
-  dummy.forEach((item) => {
+  items.forEach((item, index) => {
     $mainTable.insertAdjacentHTML('beforeend', tableInfoTemplate(item));
   });
 };
 
+const buttonTemplate = (number) => {
+  return `<button id="number-button">${number}</button>`;
+};
+
+const getItemLength = () => {
+  if (items % 10 === 0) {
+    return Math.floor(items.length / 10);
+  }
+  return Math.floor(items.length / 10) + 1;
+};
+
 const renderButton = () => {
-  //
+  const $buttonContainer = document.querySelector('#button-container');
+  const length = getItemLength();
+
+  for (let i = 1; i <= length; i++) {
+    $buttonContainer.insertAdjacentHTML('beforeend', buttonTemplate(i));
+  }
 };
 
 window.onload = () => {
+  renderButton();
   renderTable();
 };
