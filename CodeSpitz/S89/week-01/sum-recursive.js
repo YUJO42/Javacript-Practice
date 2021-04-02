1810ae6169fbc8f331ebb6f733607483ad6ebb36
yujo <bedro27@gmail.com>
2021-04-02 18:16:13 +0900

    refactor: 코드스피츠 week01 리팩터링

--- CodeSpitz/S89/week-01/sum-recursive.js
+++ CodeSpitz/S89/week-01/sum-recursive.js
@@ -1,7 +1,24 @@
 const arr = Array.from({ length: 10 }, (_, i) => i + 1);
+const arr_empty = [];
+const arr_string = ["R", "E", "C", "U", "R", "S", "I", "V", "E"];
 
-const sumArr = ([...arr]) => {
-  return arr.length && arr.pop() + sumArr(arr);
-};
+if (1) {
+  // const _recursive = (arr, i, acc) => {
+  //   if (i > 0) {
+  //     return _recursive(arr, i - 1, arr[i] + acc);
+  //   }
 
-console.log(sumArr(arr));
+  //   return arr[0] ? arr[0] + acc : acc;
+  // };
+
+  const _recursive = (arr, i, acc) =>
+    i > 0 ? _recursive(arr, i - 1, arr[i] + acc) : (arr[0] || 0) + acc;
+
+  const recursive = (arr) => {
+    _recursive(arr, arr.length - 1, 0);
+  };
+
+  console.log(recursive(arr));
+  console.log(recursive(arr_empty));
+  console.log(recursive(arr_string));
+}
